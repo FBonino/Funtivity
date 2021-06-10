3e115aad5c9718c7bd8866bb92b2c97dc896a23d
unknown <boninoffranco@gmail.com>
2021-06-10 10:24:12 -0300

    Fix routes

--- api/src/routes/countriesRoutes.js
+++ api/src/routes/countriesRoutes.js
@@ -5,13 +5,15 @@ const { Op } = require('sequelize');
 const countriesRoutes = Router();
 
 countriesRoutes.get('/', async (req, res) => {
-	let query = req.query.name;
-	let countries = 
-		query ? await Country.findAll({
+	let query = req.query.name ? req.query.name[0].toUpperCase() + req.query.name.slice(1) : undefined;
+	let countries = query 
+		? await Country.findAll({
+			attributes: {exclude: ['createdAt', 'updatedAt']},
 			where: {name: {[Op.like]: `%${query}%`}},
 			limit: 10
 		})
 		: await Country.findAll({
+			attributes: {exclude: ['createdAt', 'updatedAt']},
 			limit: 10
 		})
 	return countries.length ? res.json(countries) : res.status(400).send();
@@ -20,8 +22,12 @@ countriesRoutes.get('/', async (req, res) => {
 countriesRoutes.get('/:id', async (req, res) => {
 	let requestedID = req.params.id.toUpperCase();
 	let country = await Country.findAll({
+		attributes: {exclude: ['createdAt', 'updatedAt']},
 		where: {id: {[Op.eq]: requestedID}},
-		include: Activity
+		include: {
+			model: Activity,
+			attributes: {exclude: ['createdAt', 'updatedAt']}
+		}
 	})
 	return country.length ? res.json(country) : res.status(400).send();
 })
