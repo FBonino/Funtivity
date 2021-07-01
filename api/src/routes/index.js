debe668bad76008325016d48329e12801e692674
unknown <boninoffranco@gmail.com>
2021-07-01 18:04:35 -0300

    Cleaner code, remove unnecessary files and comments

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,10 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 const startingRoute = require("./startingRoute");
 const countriesRoutes = require("./countriesRoutes");
 const activityRoutes = require("./activityRoutes");
 
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 router.use("/start", startingRoute);
 router.use("/countries", countriesRoutes);
 router.use("/activity", activityRoutes);
