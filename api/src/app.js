b58568604b74eb81cbb06b0f74058aa0c3bb8e45
unknown <boninoffranco@gmail.com>
2021-07-07 15:11:35 -0300

    Fix CORS errors

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/');
+const cors = require('cors');
 
 require('./db.js');
 
@@ -8,16 +9,17 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cors());
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://funtivity.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 
 server.use(router);
 
