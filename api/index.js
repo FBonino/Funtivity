2ce43cce8d0a8833e9f898fd1ed32ea0bc8d88f2
unknown <boninoffranco@gmail.com>
2021-07-07 15:55:17 -0300

    Fix CORS errors

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT || 3000, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001');
   });
 }).catch(err => console.log(err));
